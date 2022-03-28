004590b3fe114cedcb779582915c0984ebed13b7
Alessandro Ferro <49845537+xfarrow@users.noreply.github.com>
2022-03-28 18:32:58 +0200

    Update keyboard.js

--- latexify/keyboard/js/keyboard.js
+++ latexify/keyboard/js/keyboard.js
@@ -32,7 +32,7 @@ const Keyboard = {
         document.body.appendChild(this.elements.main);
 
         // Automatically use keyboard for elements with .use-keyboard-input
-        document.querySelectorAll(".use-keyboard-input").forEach(element => {
+        document.querySelectorAll("input, textarea").forEach(element => {
             element.addEventListener("focus", () => {
                 this.open(element.value, currentValue => {
                     element.value = currentValue;
